65341f581f159547d1e5642ca7cc183e2de61904
vedranb <vedranb@52d03122-7f4a-0410-8706-df1158c28ff4>
2008-04-11 21:30:14 +0000

    Basic directory setup. Screens added.

---
+++ trunk/Code/Menu/Background.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Scatter.Core;
+using Scatter.Logic;
+
+namespace Scatter.Menu
+{
+    class Background : Level
+    {
+        ContentManager content;
+        Texture2D backgroundTexture;
+
+        public Background()
+        {
+            TransitionOnTime = TimeSpan.FromSeconds(0.5);
+            TransitionOffTime = TimeSpan.FromSeconds(0.5);
+        }
+
+        public override void LoadContent()
+        {
+            if (content == null)
+                content = new ContentManager(Director.Game.Services);
+
+           // backgroundTexture = content.Load<Texture2D>(@"Content\Graphics\menu\starfield");
+        }
+
+        public override void UnloadContent()
+        {
+            content.Unload();
+        }
+
+        public override void Update(GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
+        {
+            base.Update(gameTime, otherScreenHasFocus, false);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            Rectangle fullscreen = new Rectangle(0, 0, 1600, 1200);
+            byte fade = TransitionAlpha;
+        }
+
+    }
+}
---
+++ trunk/Code/Menu/MenuScreen.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using F2D.Graphics.Gui;
+using F2D.Core;
+using F2D.Input;
+
+namespace Scatter.Menu
+{
+    abstract class MenuScreen : GameScreen
+    {
+
+        List<MenuEntry> menuEntries = new List<MenuEntry>();
+        int selectedEntry = 0;
+        string menuTitle;
+
+        protected IList<MenuEntry> MenuEntries
+        {
+            get { return menuEntries; }
+        }
+
+        public MenuScreen(string menuTitle)
+        {
+            this.menuTitle = menuTitle;
+
+            TransitionOnTime = TimeSpan.FromSeconds(0.5);
+            TransitionOffTime = TimeSpan.FromSeconds(0.5);
+        }
+
+        public override void HandleInput(InputState input)
+        {
+            /*// Move to the previous menu entry?
+            if (input.MenuUp)
+            {
+                selectedEntry--;
+
+                if (selectedEntry < 0)
+                    selectedEntry = menuEntries.Count - 1;
+            }
+            // Move to the next menu entry?
+            if (input.MenuDown)
+            {
+                selectedEntry++;
+
+                if (selectedEntry >= menuEntries.Count)
+                    selectedEntry = 0;
+            }
+
+            // Accept or cancel the menu?
+            if (input.MenuSelect)
+            {
+                OnSelectEntry(selectedEntry);
+            }
+            else if (input.MenuCancel)
+            {
+                OnCancel();
+            }*/
+
+            //Override keyboard input with Mouse: it gets priority
+            for (int i = 0; i < MenuEntries.Count; i++)
+            {
+                MenuEntries[i].Button.Update();
+
+                if (MenuEntries[i].Button.CurState == F2D.Graphics.Gui.Button.State.Hover)
+                {
+                    selectedEntry = i;
+                }
+
+                else if (MenuEntries[i].Button.CurState == F2D.Graphics.Gui.Button.State.Depressed)
+                {
+                    selectedEntry = i;
+                    OnSelectEntry(selectedEntry);
+                }
+            }
+        }
+
+        protected virtual void OnSelectEntry(int entryIndex)
+        {
+            menuEntries[selectedEntry].OnSelectEntry();
+        }
+
+        protected virtual void OnCancel()
+        {
+            ExitScreen();
+        }
+
+        protected void OnCancel(object sender, EventArgs e)
+        {
+            this.UnloadContent();
+            OnCancel();
+        }
+
+        public override void Update(GameTime gameTime, bool otherScreenHasFocus,
+                                                       bool coveredByOtherScreen)
+        {
+            base.Update(gameTime, otherScreenHasFocus, coveredByOtherScreen);
+
+            // Update each nested MenuEntry object.
+            for (int i = 0; i < menuEntries.Count; i++)
+            {
+                bool isSelected = IsActive && (i == selectedEntry);
+
+                menuEntries[i].Update(this, isSelected, gameTime);
+            }
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            Director.Game.Window.Title = "GOT HERE";
+            Vector2 position = new Vector2(1100, 250);
+
+            // Make the menu slide into place during transitions, using a
+            // power curve to make things look more interesting (this makes
+            // the movement slow down as it nears the end).
+            float transitionOffset = (float)Math.Pow(TransitionPosition, 2);
+
+            if (ScreenState == ScreenState.TransitionOn)
+                position.X -= transitionOffset * 256;
+            else
+                position.X += transitionOffset * 512;
+
+            // Draw each menu entry in turn.
+            for (int i = 0; i < menuEntries.Count; i++)
+            {
+                MenuEntry menuEntry = menuEntries[i];
+                bool isSelected = IsActive && (i == selectedEntry);
+                position.Y += menuEntry.GetHeight(this);
+                menuEntry.Draw(this, position, isSelected, gameTime);
+            }
+        }
+    }
+}
