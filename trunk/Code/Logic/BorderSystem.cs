1ed4cc193bf7885fdda0369d5a03ec9e71f23ba9
steven.j.jackson <steven.j.jackson@52d03122-7f4a-0410-8706-df1158c28ff4>
2008-04-13 05:49:03 +0000

    orb emitter basics

--- trunk/Code/Logic/BorderSystem.cs
+++ trunk/Code/Logic/BorderSystem.cs
@@ -20,12 +20,16 @@ namespace Scatter.Logic
         public BorderTile[] left;
         public BorderTile[] right;
 
+        List<OrbEmitter> orbEmitters = new List<OrbEmitter>();
+
         public BorderSystem()
         {
             top = new BorderTile[16];
             bottom = new BorderTile[16];
             left = new BorderTile[10];
             right = new BorderTile[10];
+
+
         }
 
         public void Initialize()
@@ -56,6 +60,70 @@ namespace Scatter.Logic
                 right[i].Initialize(new Vector2(1550, 150 + (i * 100)), BorderType.CLCR);
                 right[i].sprite.Origin = new Vector2(right[i].sprite.Size.X / 2, right[i].sprite.Size.Y / 2);
                 right[i].sprite.Rotation = 3 * (float)Math.PI / 2;
+            }
+
+            top[8].ResetType(BorderType.OLCR);
+            top[9].ResetType(BorderType.Hole);
+            top[10].ResetType(BorderType.CLOR);
+
+            right[5].ResetType(BorderType.RedOrbEmitter);
+            left[4].ResetType(BorderType.BlueOrbEmitter);
+            bottom[2].ResetType(BorderType.GreenOrbEmitter);
+
+            top[5].ResetType(BorderType.BlueTube);
+            bottom[4].ResetType(BorderType.GreenTube);
+            bottom[8].ResetType(BorderType.RedTube);
+            
+            /* loop through and attach appropriate orb emitters */
+            for (int i = 0; i < top.Length; i++)
+            {
+                if (top[i].borderType == BorderType.RedOrbEmitter ||
+                   top[i].borderType == BorderType.BlueOrbEmitter ||
+                   top[i].borderType == BorderType.GreenOrbEmitter)
+                {
+                    OrbEmitter oe = new OrbEmitter();
+                    oe.position = top[i].sprite.Position;
+                    orbEmitters.Add(oe);
+                }
+
+                if (bottom[i].borderType == BorderType.RedOrbEmitter ||
+                   bottom[i].borderType == BorderType.BlueOrbEmitter ||
+                   bottom[i].borderType == BorderType.GreenOrbEmitter)
+                {
+                    OrbEmitter oe = new OrbEmitter();
+                    oe.position = bottom[i].sprite.Position;
+                    orbEmitters.Add(oe);
+                }
+            } 
+            
+            for (int i = 0; i < left.Length; i++)
+            {
+                if (left[i].borderType == BorderType.RedOrbEmitter ||
+                   left[i].borderType == BorderType.BlueOrbEmitter ||
+                   left[i].borderType == BorderType.GreenOrbEmitter)
+                {
+                    OrbEmitter oe = new OrbEmitter();
+                    oe.position = left[i].sprite.Position;
+                    orbEmitters.Add(oe);
+                }
+
+                if (right[i].borderType == BorderType.RedOrbEmitter ||
+                   right[i].borderType == BorderType.BlueOrbEmitter ||
+                   right[i].borderType == BorderType.GreenOrbEmitter)
+                {
+                    OrbEmitter oe = new OrbEmitter();
+                    oe.position = right[i].sprite.Position;
+                    orbEmitters.Add(oe);
+                }
+            }
+            
+        }
+
+        public void Update()
+        {
+            for (int i = 0; i < orbEmitters.Count; i++)
+            {
+                orbEmitters[i].Update();
             }
         }
 
